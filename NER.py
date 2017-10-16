a0887cae122efc7a5c42e8cbb779802dc51051e0
elliottyan <459800873@qq.com>
2017-10-16 17:19:35 +0800

    modify the data_structure with clearer understanding for how to utilize train & test data.

--- NER.py
+++ NER.py
@@ -1,6 +1,7 @@
 import tensorflow as tf
 import numpy as np
 from model import Model
+import os
 
 
 class Config(object):
@@ -9,8 +10,11 @@ class Config(object):
     n_relation = 53
     dropout = 0.5
     lr = 0.01
-    embed_size = 50
+    w_embed_size = 50
+    p_embed_size = 10
     n_epochs = 50
+    n_features = w_embed_size + 2 * p_embed_size
+
 
 class NRE_Model(Model):
 
@@ -20,4 +24,23 @@ class NRE_Model(Model):
         self.config = config
         self.build()
 
+    def add_placeholders(self):
+        n_features = self.config.n_features
+        # self.input_placeholder = tf.placeholder()
+
+
+def main():
+    # read in data
+    path = "./data/"
+    word_embedding = np.load(path+ "vec.npy")
+    train_sen = np.load(path + "train_sen")
+    train_ans = np.load(path + "train_ans")
+    test_sen = np.load(path + "test_sen")
+    test_ans = np.load(path + "test_ans")
+    train_target = np.load(path+"train_target")
+    test_target = np.load(path + "test_target")
+
+
+if __name__ == "__main__":
+    main()
 
